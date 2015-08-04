5f5a011dfc96c88a0b9a8419e3680f0a533938f8
dronni3de <dronni3de@gmail.com>
2015-08-04 17:18:38 +0300

    fix

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+   $('body.page-lead .page-header-1').anystretch("img/top-image-1.jpg", {speed: 150}); 
+   $('article.page-header-1.v2').anystretch("img/top-image-2.jpg", {speed: 150}); 
+   $('body.page-sales article.personal-trainer').anystretch("img/top-image-3.jpg", {speed: 150});  
+   $('body.page-sales article.boxer').anystretch("img/top-image-4.jpg", {speed: 150});	
+	
+});
